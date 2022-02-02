46b4346817988f3d26f2fc1836133efc35c497a1
MrAndreID <andreaadam215@gmail.com>
2022-02-02 12:35:31 +0700

    add structs for response & request

---
+++ structs/request.go
@@ -0,0 +1,12 @@
+package structs
+
+type (
+	Request struct {
+		Page    string `json:"page" query:"page"`
+		Limit   string `json:"limit" query:"limit"`
+		OrderBy string `json:"orderBy" query:"orderBy"`
+		SortBy  string `json:"sortBy" query:"sortBy"`
+		Draw    string `json:"draw" query:"draw"`
+		Search  string `json:"search" query:"search"`
+	}
+)
