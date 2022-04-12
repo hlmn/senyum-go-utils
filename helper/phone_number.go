13169e6c377c9a14b4b5ec8fb36f9d670b045f2d
Muhammad Hilman <hlmn.hg@gmail.com>
2022-04-12 11:07:27 +0700

    len 0 handling

--- helper/phone_number.go
+++ helper/phone_number.go
@@ -7,6 +7,9 @@ func convertPhoneNumberTo08(phoneNumber *string) {
 		return
 	}
 	var phoneNumberTemp = *phoneNumber
+	if len(phoneNumberTemp) == 0 {
+		return
+	}
 	if phoneNumberTemp[0:2] == "62" {
 		*phoneNumber = strings.Replace(phoneNumberTemp, "62", "0", 1)
 		return
