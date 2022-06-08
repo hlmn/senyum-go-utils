d9e97aa973f55202eafea72acc52d279eafb5aaa
MrAndreID <andreaadam215@gmail.com>
2022-06-08 16:17:45 +0700

    add validator for float between

--- validator/validator.go
+++ validator/validator.go
@@ -3,7 +3,10 @@ package config
 import (
 	"encoding/base64"
 	"errors"
+	"fmt"
 	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/thedevsaddam/govalidator"
@@ -51,6 +54,56 @@ func InitValidator() {
 		_, err := base64.StdEncoding.DecodeString(val)
 		if err != nil {
 			return errors.New("The " + field + " is not base64 format")
+		}
+
+		return nil
+	})
+
+	govalidator.AddCustomRule("float_between", func(field string, rule string, message string, value interface{}) error {
+		val, ok := value.(string)
+		if !ok {
+			return fmt.Errorf("The %s field is not a string", field)
+		}
+
+		if val == "" {
+			return nil
+		}
+
+		data, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			return fmt.Errorf("The %s field is not a float", field)
+		}
+
+		ruleData := strings.TrimPrefix(rule, "float_between:")
+
+		if len(strings.Split(ruleData, ",")) != 2 {
+			return fmt.Errorf("The %s field is invalid rule", field)
+		}
+
+		if strings.Split(ruleData, ",")[0] != "" {
+			minValue := strings.Split(ruleData, ",")[0]
+
+			min, err := strconv.ParseFloat(minValue, 64)
+			if err != nil {
+				return fmt.Errorf("The %s field is not a float for min value", field)
+			}
+
+			if data < min {
+				return fmt.Errorf("The %s field must be minimum %f", field, min)
+			}
+		}
+
+		if strings.Split(ruleData, ",")[1] != "" {
+			maxValue := strings.Split(ruleData, ",")[1]
+
+			max, err := strconv.ParseFloat(maxValue, 64)
+			if err != nil {
+				return fmt.Errorf("The %s field is not a float for max value", field)
+			}
+
+			if data > max {
+				return fmt.Errorf("The %s field must be maximum %f", field, max)
+			}
 		}
 
 		return nil
